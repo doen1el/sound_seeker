0cf284a05c437b06b71889c4fe91f85ab0a3e16e
Daniel <github@mail.danielmuenstermann.de>
2025-07-23 16:15:26 +0200

    now!

--- web_app.py
+++ web_app.py
@@ -361,6 +361,4 @@ def handle_connect():
         socketio.emit('log_message', msg)
 
 if __name__ == '__main__':
-    load_dotenv()
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
